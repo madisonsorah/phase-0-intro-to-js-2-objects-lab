3d99a2fff83459b54eda105e9445ad2178888ef5
madison sorah <madisonsorah@gmail.com>
2021-11-03 11:52:56 -0400

    commit tree 1b329ed31eb88d6c0f2309dc66da6931f7905283

--- index.js
+++ index.js
@@ -16,12 +16,61 @@ function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
 }
 
 function deleteFromEmployeeByKey(employee, key) {
-    const newEmployeeByKey = {...employee};
-    delete newEmployeeByKey[key];
-    return newEmployeeByKey;
+    const newEmployee = {...employee};
+    delete newEmployee[key];
+    return newEmployee;
 }
 
 function destructivelyDeleteFromEmployeeByKey(employee, key) {
     delete employee[key];
     return employee;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const employee = {
+//     name: "Madison",
+//     streetAddress: "564 Saint Johns Place",
+// }
+
+// function updateEmployeeWithKeyAndValue(employee, key, value) {
+//     const newEmployee = {...employee};
+//     newEmployee[key] = value;
+//     return newEmployee;
+// }
+
+// function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+//     employee[key] = value;
+//     return employee;
+// }
+
+// function deleteFromEmployeeByKey(employee, key) {
+//     const newEmployeeByKey = {...employee};
+//     delete newEmployeeByKey[key];
+//     return newEmployeeByKey;
+// }
+
+// function destructivelyDeleteFromEmployeeByKey(employee, key) {
+//     delete employee[key];
+//     return employee;
+// }
